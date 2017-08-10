d21f430b23ef77c27d709682756ee364a36de312
Joseph <josefweel@gmail.com>
2017-08-10 14:30:25 +0200

    hoi

--- BeepSolution/BeepWPF/MainWindow.xaml.cs
+++ BeepSolution/BeepWPF/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows.Shapes;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Drawing;
 using System.Collections.Generic;
 using static System.Math;
 
@@ -41,24 +42,25 @@ namespace Beep {
                 //if (Abs(j % 2) == 1) continue;
                 //if (j < 0) continue;
 
-                for (int i = 0; i < bw.Size.X; i++) {
-                    //        Console.Write(" " + i + "," + j + " ");
-                    //Console.Write(" * ");
-                    double posX = i * HEXAGON_HORIZONTAL_LENGTH + relativeX;
+//                for (int i = 0; i < bw.Size.X; i++) {
+                for (int i = -bw.Size.X+1; i < bw.Size.X; i++) {
+                        //        Console.Write(" " + i + "," + j + " ");
+                        //Console.Write(" * ");
+                        double posX = i * HEXAGON_HORIZONTAL_LENGTH + relativeX;
 
 
                     if (j % 2 != 0) posX += HEXAGON_HORIZONTAL_HALF;
 
                     foreach (Line line in MakeHexagon(posX, posY)) {
                         canvas.Children.Add(line);
-                        Label label = new Label() {
-                            Foreground = new SolidColorBrush(Colors.Black),
-                            Content = i + "," + j,
-                            FontSize = 8,
-                            RenderTransform = new TranslateTransform { X = posX - HEXAGON_SIDE_LENGTH/4, Y = posY - HEXAGON_SIDE_LENGTH/2 }
-                        };
-                        canvas.Children.Add(label);
                     }
+                    Label label = new Label() {
+                        Foreground = new SolidColorBrush(Colors.Black),
+                        Content = i + "," + j,
+                        FontSize = 6,
+                        RenderTransform = new TranslateTransform { X = posX - HEXAGON_SIDE_LENGTH / 4, Y = posY - HEXAGON_SIDE_LENGTH / 2 }
+                    };
+                    canvas.Children.Add(label);
                 }
             //    Console.WriteLine("");
             }
@@ -66,13 +68,25 @@ namespace Beep {
 
         // returns a list of six lines representing a hexagon
         private List<Line> MakeHexagon(double posX, double posY) {
-            #region createlines
+            //List<System.Windows.Point> hexagonPoints = new List<System.Windows.Point>();
+            Polygon p = new Polygon();
+            
+            System.Windows.Point[] hexagonPoints = new System.Windows.Point[6] {
+                new System.Windows.Point(posX, posY),
+                new System.Windows.Point(posX, posY),
+                new System.Windows.Point(posX, posY),
+                new System.Windows.Point(posX, posY),
+                new System.Windows.Point(posX, posY),
+                new System.Windows.Point(posX, posY)
+            };
+
+            #region createpoints
             Line l1 = new Line() {
                 X1 = posX,
                 X2 = posX,
                 Y1 = posY,
                 Y2 = posY + HEXAGON_SIDE_LENGTH,
-                Stroke = System.Windows.Media.Brushes.LightSteelBlue
+                Stroke = Brushes.LightSteelBlue
             };
             Line l2 = new Line() {
                 X1 = posX,
@@ -109,13 +123,13 @@ namespace Beep {
                 Y2 = posY + HEXAGON_SIDE_LENGTH + HEXAGON_VERTICAL_EDGE,
                 Stroke = System.Windows.Media.Brushes.LightSteelBlue
             };
-            #endregion createlines
+            #endregion
             return new List<Line>() { l1, l2, l3, l4, l5, l6 };
         }
         
         private void OnMouseMove(object sender, System.Windows.Input.MouseEventArgs e) {
             var p = e.GetPosition(this);
-            MouseText.Text = p.X + " , " + p.Y;
+            MouseText.Text = (int)(p.X) + " , " + (int)(p.Y);
         }
     }
 }
