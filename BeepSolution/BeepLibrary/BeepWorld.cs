612e49e4877fb9f217aa7708ea94b0fffa7433fb
Joseph <josefweel@gmail.com>
2017-08-11 14:20:31 +0200

    library wasnt pushed for some reason

---
+++ BeepSolution/BeepLibrary/BeepWorld.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+//using System.Drawing; // Point structure
+
+// TODO
+// -generate painting
+// -possible database functionality => save random generated painting seed/parameters to database
+
+namespace Beep {
+    // this class is a generated painting
+    public class BeepWorld {
+
+        public Point Size { get; set; }
+        public Dictionary<Point, Tile> tiles;
+
+        public BeepWorld(Point p) : this(p, false) { }
+        public BeepWorld(int sizeX, int sizeY) : this(new Point(sizeX, sizeY), false) { }
+
+        public BeepWorld(Point p, bool boxed) {
+            Size = p;
+            tiles = new Dictionary<Point, Tile>();
+            for (int indexY = 0; indexY < Size.Y; indexY++) {
+
+                int startX = 0 - (indexY / 2);
+                int endX = Size.X + startX;
+
+                if (indexY % 2 != 0) endX--; // this line makes the grid a box if SizeY is even
+
+                for (int indexX = startX; indexX < endX; indexX++) {
+                    tiles.Add(new Point(indexX, indexY), new Tile(indexX, indexY));
+                }
+            }
+        }
+    }
+
+    public class Tile {
+        public List<Tile> Neighbors { get; set; }
+        public Point Coordinates { get; set; }
+        public Brush Color { get; set; }
+        internal Tile(int x, int y) {
+            this.Coordinates = new Point(x, y);
+        }
+        internal Tile(Point p) {
+            this.Coordinates = p;
+        }
+    }
+
+    public struct Point {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Point(int x, int y) {
+            X = x;
+            Y = y;
+        }
+    }
+}
