e3094f8e1c1be09ee3997958e58d405d05b2b734
Joseph <josefweel@gmail.com>
2017-08-21 14:33:21 +0200

    new mouse functionality, some code refactoring, logic, start on vincent rule

---
+++ BeepSolution/BeepWPF/RuleUI/VincentRuleUserControl.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Beep.Rules;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Media;
+using Xceed.Wpf.Toolkit;
+
+namespace Beep.RuleUI {
+    /// <summary>
+    /// Interaction logic for ChangeNeighbor.xaml
+    /// </summary>
+    public partial class VincentRuleUserControl : BeepRuleUserControl {
+
+        public override string SelectedRuleName {
+            get { return comboBoxRulePicker.SelectedItem as string; }
+        }
+
+        public override void UpdateColorPickers(ObservableCollection<ColorItem> usedColorItems) {
+            clrPickMatch.AvailableColors = usedColorItems;
+            clrPickTarget.AvailableColors = usedColorItems;
+            clrPickIgnore.AvailableColors = usedColorItems;
+        }
+
+        public override void PrepareColorPickers(ObservableCollection<ColorItem> standardColorItems) {
+            clrPickMatch.StandardColors = standardColorItems;
+            clrPickTarget.StandardColors = standardColorItems;
+            clrPickIgnore.StandardColors = standardColorItems;
+        }
+
+        protected override void SetPanels() {
+            panelCollapsed = pnlCollapsed;
+            panelExpanded = pnlExpanded;
+        }
+
+        public VincentRuleUserControl(VincentRule rule) {
+            this.Rule = rule;
+            this.ruleName = rule.RuleName;
+
+            InitializeComponent();
+            SetPanels();
+
+            clrPickMatch.SelectedColor = rule.MatchColor;
+            clrPickTarget.SelectedColor = rule.TargetColor;
+            //clrPickIgnore.SelectedColor = rule.IgnoreColor;
+        }
+
+        private void ClrPickMatchChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e) {
+            (Rule as VincentRule).MatchColor = (Color)e.NewValue;
+        }
+
+        private void ClrPickTargetChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e) {
+            (Rule as VincentRule).TargetColor = (Color)e.NewValue;
+        }
+
+        private void ClrPickIgnoreChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e) {
+            //(Rule as VincentRule).IgnoreColor = (Color)e.NewValue;
+        }
+    }
+}
